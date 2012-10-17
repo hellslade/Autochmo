93a1708f93d27f1ac750503a5e6a548326d7b66e
Grigorev Aleksandr <grigorev.aleksandr.a@gmail.com>
2012-10-17 14:04:41 +0600

    Переделан PullToRefresh на более стабильный от Chris Banes

--- src/ru/android/hellslade/autochmo/LentaActivity.java
+++ src/ru/android/hellslade/autochmo/LentaActivity.java
@@ -2,13 +2,12 @@ package ru.android.hellslade.autochmo;
 
 import java.util.List;
 
-import android.app.ListActivity;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Toast;
@@ -16,96 +15,87 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
-import com.markupartist.android.widget.PullToRefreshListView;
-import com.markupartist.android.widget.PullToRefreshListView.OnRefreshListener;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnLastItemVisibleListener;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
 
 public class LentaActivity extends SherlockFragmentActivity {
-    class GetFactTask extends AsyncTask<Integer, Integer, List<Fact>> {
-    	ProgressDialog pg = new ProgressDialog(LentaActivity.this);
-    	@Override
-    	protected void onPreExecute() {
-        	pg.setMessage(getApplicationContext().getResources().getString(R.string.get_fact_task_message));
-        	pg.show();
-    		super.onPreExecute();
-    	}
+    class GetFactTask extends AsyncTask<Integer, Void, List<Fact>> {
 		@Override
 		protected List<Fact> doInBackground(Integer... params) {
-			final int count = Integer.valueOf(settings.getString(getString(R.string.factCountKey), factCount_default));
-	        facts = mAutochmo._getFactList(offset, count);
+			if (params.length > 0 && params[0] != null) 
+			    mOffset = params[0];
+			List<Fact> facts = mAutochmo._getFactList(mOffset, mLimit);
 	        if (facts == null)
 	        {
 	            Toast.makeText(LentaActivity.this, R.string.connection_failed, Toast.LENGTH_LONG).show();
 	            return null;
 	        }
-	        // ================
 			return facts;
-	        
 		}
     	@Override
     	protected void onPostExecute(List<Fact> result) {
     		setAdapterToList(result);
-    		pg.dismiss();
     		super.onPostExecute(result);
     	}
     }
-    private View mRefreshView;
-    /** Called when the activity is first created. */
     public SharedPreferences settings;
-    FactListAdapter listAdapter;
-    public List<Fact> facts;
+    FactListAdapter mAdapter;
     public String passwordHash;
-    private int offset;
-    private String factCount_default;
+    private int mOffset = 0;
+    private int mLimit;
     private AutochmoApplication mAutochmo;
-    private ListView imageListView;
+    private PullToRefreshListView imageListView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lenta);
+        imageListView = (PullToRefreshListView)findViewById(R.id.lentaListView);
+        imageListView.setOnRefreshListener(new OnRefreshListener<ListView>() {
 
-        // Set a listener to be invoked when the list should be refreshed.
-        ((PullToRefreshListView) getListView()).setOnRefreshListener(new OnRefreshListener() {
-            @Override
-            public void onRefresh(View v) {
-                // Do work to refresh the list here.
-                mRefreshView = v;
-                new GetDataTask().execute();
-            }
-        });
+			@Override
+			public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+				if (mAdapter != null) {
+        			mAdapter.clear();
+        		}
+				imageListView.setLastUpdatedLabel(DateUtils.formatDateTime(mAutochmo,
+						System.currentTimeMillis(), DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_SHOW_DATE
+								| DateUtils.FORMAT_ABBREV_ALL));
+        		new GetFactTask().execute(0);
+			}
+		});
+        imageListView.setOnLastItemVisibleListener(new OnLastItemVisibleListener() {
+        	@Override
+        	public void onLastItemVisible() {
+        		new GetFactTask().execute(mOffset + mLimit);
+        	}
+		});
         settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         mAutochmo = (AutochmoApplication)getApplication();
-        offset = 0;
-        factCount_default = "30";
-        imageListView = getListView();
-        settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        imageListView.setOnItemClickListener(itemClickListener);
+        mLimit = Integer.valueOf(settings.getString(getString(R.string.factCountKey), "30"));
+        imageListView.getRefreshableView().setOnItemClickListener(itemClickListener);
         // Получить список фактов
-        //_getFactList(offset);
-        new GetFactTask().execute(offset);
-    }
-    public void _getFactList(int offset)
-    {
-        final int count = Integer.valueOf(settings.getString(getString(R.string.factCountKey), factCount_default));
-        facts = mAutochmo._getFactList(offset, count);
-        if (facts == null)
-        {
-            Toast.makeText(this, R.string.connection_failed, Toast.LENGTH_LONG).show();
-            return;
-        }
-        // ================
-        setAdapterToList(facts);
+        imageListView.setRefreshing(true);
+        new GetFactTask().execute(0);
     }
     public void setAdapterToList(List<Fact> facts) {
-        imageListView.setAdapter(null);
-        FactListAdapter listAdapter = new FactListAdapter(LentaActivity.this, facts, imageListView);
-        imageListView.setAdapter(listAdapter);
-        imageListView.invalidateViews();
+    	if (mAdapter == null) {
+			mAdapter = new FactListAdapter(LentaActivity.this, facts, imageListView.getRefreshableView());
+			imageListView.getRefreshableView().setAdapter(mAdapter);
+		} else {
+			for (Fact f : facts) {
+				mAdapter.add(f);
+			}
+//			mAdapter.notifyDataSetChanged();
+		}
+    	imageListView.onRefreshComplete();
     }
     OnItemClickListener itemClickListener = new OnItemClickListener()
     {
         public void onItemClick(AdapterView listView, View view, int pos, long id) {
-            Fact f = facts.get(pos - 1);
+            Fact f = mAdapter.getItem(pos - 1);
             List<Comment> comments = mAutochmo._getComment(f.getFactId());
             Intent intent = new Intent();
             intent.setClass(LentaActivity.this, FactActivity.class);
@@ -117,41 +107,5 @@ public class LentaActivity extends SherlockFragmentActivity {
             }
             startActivity(intent);
         }
-
     };
-    private class GetDataTask extends AsyncTask<Void, Void, List<Fact>> {
-
-        @Override
-        protected List<Fact> doInBackground(Void... params) {
-            // Simulates a background job.
-            try {
-                final int count = Integer.valueOf(settings.getString(getString(R.string.factCountKey), factCount_default));
-                if (mRefreshView.getId() == R.id.pull_to_refresh_footer) {
-                    offset += count;
-                } else {
-                    offset -= count;
-                }
-                offset = offset >= 0 ? offset : 0;
-                facts = mAutochmo._getFactList(offset, count);
-            } catch (Exception e) {
-                ;
-            }
-            return facts;
-        }
-        
-        @Override
-        protected void onPostExecute(List<Fact> result) {
-            //mListItems.addFirst("Added after refresh...");
-            facts = result;
-            setAdapterToList(facts);
-            // Call onRefreshComplete when the list has been refreshed.
-            ((PullToRefreshListView) getListView()).onRefreshComplete(mRefreshView);
-
-            super.onPostExecute(result);
-        }
-    }
-    public PullToRefreshListView getListView() {
-        PullToRefreshListView list = (PullToRefreshListView) findViewById(R.id.lentaListView);
-        return list;
-    }
 }
